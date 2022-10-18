378b465bce928f3eabce527ed080607ae50f5035
Hong Hanh Chu <honghanhchu@Hong-Hanh-Mac-Mini-M1.local>
2022-10-18 15:26:47 +0200

    slideshow-component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SlideshowComponent } from './slideshow/slideshow.component';
-import { NewCompComponent } from './new-comp/new-comp.component';
-import { HelloComponent } from './hello/hello.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     SlideshowComponent,
-    NewCompComponent,
-    HelloComponent
   ],
   imports: [
     BrowserModule,
